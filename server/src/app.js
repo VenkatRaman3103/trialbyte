94e4b91c878e0e8f6b15864e179eae356f34c5fd
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-07-23 11:55:00 +0530

    feat(trials): base route for trials

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import { userRouter } from './routes/user.js';
+import { trialsRouter } from './routes/trials.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', userRouter);
+
+app.use('/api', trialsRouter);
 
 export default app;
--- server/src/routes/trials.js
+++ server/src/routes/trials.js
@@ -5,7 +5,12 @@ import { eq, inArray } from 'drizzle-orm';
 
 export const trialsRouter = Router();
 
+trialsRouter.get('/trials', async (req, res) => {
+    res.json('Hello from trials');
+});
+
 // NOTE: create trials
+
 // NOTE: read trials
 // NOTE: update trials
 // NOTE: delete trials
