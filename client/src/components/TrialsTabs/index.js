36f0fcac786e90699ec63a3c7f35dd6de3e37338
VenkatRaman3103 <venkat.raman.3103@gmail.com>
2025-07-27 20:08:56 +0530

    feat(TrialsTabs):added StudyPublication components

--- client/src/components/TrialsTabs/index.js
+++ client/src/components/TrialsTabs/index.js
@@ -148,6 +148,12 @@ export const TrialsTabs = () => {
 
                     {/* content section */}
                     <div className="trial-contents-section">
+                        <Section>
+                            <StudyPublication />
+                        </Section>
+                        <Section>
+                            <TrialPublish />
+                        </Section>
                         <Section heading={"trial-contents-section"}>
                             <div className="trial-outcome-container">
                                 <div className="trial-outcome-left-container">
@@ -725,6 +731,133 @@ const ClinicalTrialTimeline = () => {
                     </div>
                 </div>
             </div>
+        </div>
+    );
+};
+
+const TrialPublish = () => {
+    return (
+        <div className="trialPublish-container">
+            <div className="trialPublish-card">
+                <div className="trialPublish-header">
+                    <div>
+                        <span className="trialPublish-header-label">
+                            Date :
+                        </span>{" "}
+                        October 23, 2021
+                    </div>
+                    <div>
+                        <span className="trialPublish-header-label">
+                            Result Type :
+                        </span>{" "}
+                        Full Results
+                    </div>
+                    <div>
+                        <span className="trialPublish-header-label">
+                            Source :
+                        </span>{" "}
+                        PubMed
+                        <span className="trialPublish-infoIcon">
+                            &#9432;
+                        </span>{" "}
+                    </div>
+                </div>
+
+                <h2 className="trialPublish-title">
+                    Efficacy and safety of long-acting pasireotide or everolimus
+                    alone or in combination in patients with advanced carcinoids
+                    of the lung and thymus (LUNA):
+                </h2>
+                <div className="trialPublish-content">
+                    <p className="trialPublish-authors">
+                        Piero Ferolla, Maria Pia Brizzi, Tim Meyer, Wasat
+                        Mansoor, Julien Mazieres, Christine Do Cao, Hervé Léna,
+                        Alfredo Berruti
+                    </p>
+
+                    <div className="trialPublish-section">
+                        <h3 className="trialPublish-sectionTitle">Results :</h3>
+                        <p className="trialPublish-sectionText">
+                            11 patients died during the core 12-month treatment
+                            phase or up to 56 days after the last study
+                            treatment exposure date: two (5%) of 41 in the
+                            long-acting pasireotide group, six (14%) of 42 in
+                            the everolimus group, and three (7%) of 41 in the
+                            combination group. No deaths were suspected to be
+                            related to long-acting pasireotide treatment. One
+                            death in the everolimus group (acute kidney injury
+                            associated with diarrhoea), and two deaths in the
+                            combination group (diarrhoea and urinary sepsis in
+                            one patient, and acute renal failure and respiratory
+                            failure in one patient) were suspected to be related
+                            to everolimus treatment. In the latter patient,
+                            acute renal failure was not suspected to be related
+                            to everolimus treatment, but respiratory failure was
+                            suspected to be related.
+                        </p>
+                    </div>
+
+                    <div className="trialPublish-conclusionSection">
+                        <h3 className="trialPublish-sectionTitle">
+                            Conclusion :
+                        </h3>
+                        <p className="trialPublish-sectionText">
+                            The study met the primary endpoint in all three
+                            treatment groups. Safety profiles were consistent
+                            with the known safety profiles of these agents.
+                            Further studies are needed to confirm the antitumour
+                            efficacy of the combination of a somatostatin
+                            analogue with everolimus in lung and thymic
+                            carcinoids
+                        </p>
+                    </div>
+
+                    <div className="trialPublish-footer">
+                        <button className="trialPublish-button trialPublish-button-primary">
+                            View source
+                        </button>
+                        <button className="trialPublish-button trialPublish-button-secondary">
+                            Full Text
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const StudyPublication = () => {
+    return (
+        <div className="studyPublication-card">
+            <div className="studyPublication-header">
+                <div>
+                    <span className="studyPublication-header-label">
+                        Date :
+                    </span>{" "}
+                    January 25, 2020
+                </div>
+                <div>
+                    <span className="studyPublication-header-label">
+                        Result Type :
+                    </span>{" "}
+                    Interim Results
+                </div>
+                <div>
+                    <span className="studyPublication-header-label">
+                        Source :
+                    </span>{" "}
+                    PubMed
+                    <span className="studyPublication-infoIcon">
+                        &#x2b; {/* Plus sign icon */}
+                    </span>{" "}
+                </div>
+            </div>
+
+            <h2 className="studyPublication-title">
+                A Multicenter Randomized Phase III Study of Single Agent
+                Efficacy and Optimal Combination Sequence of Everolimus and
+                Pasireotide LAR in Advanced Thyroid Cancer.
+            </h2>
         </div>
     );
 };
